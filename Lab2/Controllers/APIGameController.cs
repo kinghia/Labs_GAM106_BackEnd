47b1a64ececf95486b2394004bdc397485e0b030
kinghia <162483327+kinghia@users.noreply.github.com>
2024-11-23 17:17:04 +0700

    update labs

--- Lab2/Controllers/APIGameController.cs
+++ Lab2/Controllers/APIGameController.cs
@@ -4,11 +4,16 @@ using Lab2.DTO;
 using Lab2.Models;
 using Lab2.Service;
 using Lab2.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Lab2.Controllers
 {
@@ -20,13 +25,42 @@ namespace Lab2.Controllers
         private readonly IEmailService _emailService;
         protected ResponseApi _response;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        private string GenerateJwtToken(ApplicationUser user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.NameIdentifier, user.Id)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
 
         public APIGameController(ApplicationDbContext db,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            IEmailService emailService,
+            IConfiguration configuration)
         {
             _db = db;
             _response = new();
             _userManager = userManager;
+            _emailService = emailService;
+            _configuration = configuration;
         }
 
         [HttpGet("GetAllGameLevel")]
@@ -138,9 +172,15 @@ namespace Lab2.Controllers
                 var user = await _userManager.FindByEmailAsync(email);
                 if (user != null && await _userManager.CheckPasswordAsync(user, password))
                 {
+                    var token = GenerateJwtToken(user);
+                    var data = new
+                    {
+                        token = token,
+                        user = user,
+                    };
                     _response.IsSuccess = true;
                     _response.Notification = "Đăng nhập thành công";
-                    _response.Data = user;
+                    _response.Data = data;
                     return Ok(_response);
                 }
                 else
@@ -380,7 +420,10 @@ namespace Lab2.Controllers
 
                     user.Avatar = fileName;
                 }
-
+                await _db.SaveChangesAsync();
+                _response.IsSuccess = true;
+                _response.Notification = "Cập nhật thông tin thành công";
+                _response.Data = user;
                 return Ok(_response);
             }
             catch (Exception ex)
@@ -543,6 +586,28 @@ namespace Lab2.Controllers
                     _response.Data = null;
                     return BadRequest(_response);
                 }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Notification = "Lỗi";
+                _response.Data = ex.Message;
+                return BadRequest(_response);
+            }
+        }
+
+        [HttpGet("GetAllResultByUser/{userId}")]
+        [Authorize]
+        public async Task<IActionResult> GetAllResultByUser(string userId)
+        {
+            try
+            {
+                var result = await _db.LevelResults.Where(x => x.UserId == userId).ToListAsync();
+
+                _response.IsSuccess = true;
+                _response.Notification = "Lấy dữ liệu thành công";
+                _response.Data = result;
+                return Ok(_response);
             }
             catch (Exception ex)
             {
